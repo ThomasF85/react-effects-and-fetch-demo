e91ff55375e9129bdce2ede823603c0ac82f66b8
ThomasF85 <thomas.foeldi@gmail.com>
2023-04-26 11:07:17 +0200

    add fetch with useEffect

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+
 export default function Joke() {
+  const [joke, setJoke] = useState();
+  const [id, setId] = useState(0);
+
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch(
+        `https://example-apis.vercel.app/api/bad-jokes/${id}`
+      );
+      const joke = await response.json();
+      setJoke(joke);
+    }
+
+    startFetching();
+  }, [id]);
+
+  if (!joke) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-      <h1>Insert joke here</h1>
-      <button type="button">Next Joke</button>
+      <h1>{joke.joke}</h1>
+      <button onClick={() => setId(joke.nextId)} type="button">
+        Next Joke
+      </button>
     </>
   );
 }
